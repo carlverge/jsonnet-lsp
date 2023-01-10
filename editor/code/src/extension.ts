a36e6273027f2db5b7328d30d6f84826f43fb5a7
Carl Verge <carlverge@gmail.com>
2023-01-09 22:39:51 -0800

    add publisher and default to bundled lsp

--- editor/code/src/extension.ts
+++ editor/code/src/extension.ts
@@ -10,21 +10,17 @@ import {
 
 let client: LanguageClient;
 
-async function startClient(): Promise<void> {
-	let cfg = workspace.getConfiguration('jsonnet.lsp');
+async function startClient(binaryPath: string): Promise<void> {
 
 	const executable: Executable = {
-		command: cfg.get('serverBinary') ?? "jsonnet-lsp",
+		command: binaryPath,
 		args: [],
-		options: {
-			env: process.env,
-		},
+		options: {env: process.env},
 	};
 
 	const serverOptions: ServerOptions = {
 		run: executable,
 		debug: executable,
-
 	};
 
 	const clientOptions: LanguageClientOptions = {
@@ -42,8 +38,13 @@ async function startClient(): Promise<void> {
 }
 
 export async function activate(context: ExtensionContext) {
+	let cfg = workspace.getConfiguration('jsonnet.lsp');
 
-	await startClient();
+	const binaryPath: string = cfg.get('binaryPath') ?? 
+		// use bundled language server if one is not provided
+		context.asAbsolutePath(`jsonnet-lsp_${process.platform}_${process.arch}`);
+
+	await startClient(binaryPath);
 
 	context.subscriptions.push(
 		commands.registerCommand('jsonnet.lsp.restart', async function (): Promise<void> {
@@ -52,7 +53,7 @@ export async function activate(context: ExtensionContext) {
 				client.outputChannel.dispose();
 			}
 
-			await startClient();
+			await startClient(binaryPath);
 		}),
 	);
 }
